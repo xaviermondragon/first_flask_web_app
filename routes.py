fb382bcbf1254dd377cd58353422c1040ae47d7c
xaviermondragon <xavierrmondragon@gmail.com>
2022-04-13 09:36:39 +0200

    Add update and delete functionality.

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from asyncio import tasks
 from crypt import methods
 from turtle import title
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from models import Task
 from datetime import date, datetime
 
@@ -21,5 +21,41 @@ def add():
         t = Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
         db.session.commit()
+        flash('Task added to the database')
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    task = Task.query.get(task_id)
+
+    if task:
+        form = forms.AddTaskForm()
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Task has been updated')
+            return redirect(url_for('index'))
+        form.title.data = task.title
+        return render_template('edit.html', form=form, task_id=task_id)
+    else:
+        flash('Task not found')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
+def delete(task_id):
+    task = Task.query.get(task_id)
+
+    if task:
+        form = forms.DeleteTaskForm()
+        if form.validate_on_submit():
+            db.session.delete(task)
+            db.session.commit()
+            flash('Task has been deleted')
+            return redirect(url_for('index'))
+
+        return render_template('delete.html', form=form, task_id=task_id, title=task.title)
+    else:
+        flash('Task not found')
+    return redirect(url_for('index'))
